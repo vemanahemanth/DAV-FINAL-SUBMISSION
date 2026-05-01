58345d1eca3b37d215d33c953efba0befc7e1e50
Vemana Hemanth Babu <vemanahemanthbabu@Vemanas-MacBook-Air.local>
2026-05-02 01:37:52 +0530

    Final cleanup of event filtering logic and UI aesthetics

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -7,17 +7,17 @@ import { logger } from "./lib/logger";
 const app: Express = express();
 
 app.use(
-  (pinoHttp as any)({
+  pinoHttp({
     logger,
     serializers: {
-      req(req: any) {
+      req(req) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res: any) {
+      res(res) {
         return {
           statusCode: res.statusCode,
         };
